99e00fd13a693e30e4dff4d5abfff908064938f6
Andrey Klyuev <ankl4378@gmail.com>
2023-10-11 12:10:04 +0700

    firs commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
